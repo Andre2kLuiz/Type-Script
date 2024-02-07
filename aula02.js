fbb7c3c31ed6f3157c27bd4641ffcc2f456d092c
André Luiz <123383093+Andre2kLuiz@users.noreply.github.com>
2024-02-06 21:53:18 -0300

    up

---
+++ aula02.js
@@ -0,0 +1,5 @@
+var nome = "Andre";
+var canal = "CFBCursos";
+var curso = "TypeScript";
+console.log(nome);
+console.log(canal);
---
+++ aula02.ts
@@ -0,0 +1,6 @@
+const nome = "Andre"
+const canal = "CFBCursos"
+const curso = "TypeScript"
+
+console.log(nome);
+console.log(canal);
