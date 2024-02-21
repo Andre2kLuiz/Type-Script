868fe49bdde1c08074a79487c719080d3196784f
Andre Luiz <andre2kluiz@gmail.com>
2024-02-21 11:14:16 -0300

    atualização TS

---
+++ build/aula12.js
@@ -0,0 +1,6 @@
+"use strict";
+function teste(user, password) {
+    console.log(`User: ${user}`);
+    console.log(`Senha: ${password}`);
+}
+teste('Andre Luiz', '123');
---
+++ src/aula12.ts
@@ -0,0 +1,6 @@
+function teste (user:string, password:string):void {
+    console.log(`User: ${user}`);
+    console.log(`Senha: ${password}`);
+}
+
+teste('Andre Luiz', '123');
