5a3cac11f1fabf48f1b95537152e4dbdb8f99d80
Andre Luiz <andre2kluiz@gmail.com>
2024-02-21 11:23:53 -0300

    aula12 commit

--- build/aula12.js
+++ build/aula12.js
@@ -4,3 +4,11 @@ function teste(user, password) {
     console.log(`Senha: ${password}`);
 }
 teste('Andre Luiz', '123');
+function soma2(n1, n2) {
+    let ret = n1 + n2;
+    return ret;
+}
+const res = soma2(10, 25);
+const s_res = soma2(33, 22).toString();
+console.log(res);
+console.log(s_res);
--- src/aula12.ts
+++ src/aula12.ts
@@ -4,3 +4,14 @@ function teste (user:string, password:string):void {
 }
 
 teste('Andre Luiz', '123');
+
+function soma2 (n1:number, n2:number):number {
+    let ret = n1 + n2
+    return ret
+}
+
+const res:number = soma2(10, 25)
+const s_res:string = soma2(33, 22).toString();
+
+console.log(res);
+console.log(s_res);
