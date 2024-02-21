eb98119c3ce3f6b05ee1df9fc437ea9186e0b05b
Andre Luiz <andre2kluiz@gmail.com>
2024-02-21 11:38:05 -0300

    aula13 ts

---
+++ src/aula13.ts
@@ -0,0 +1,15 @@
+function soma(n1:number, n2:number):number {
+    return n1 + n2;
+}
+
+console.log(soma(5, 5));
+
+function novoUser(user:string, pass:string, nome?:string):void {
+
+    let dados = [user, pass, nome]
+    console.log(dados);
+    
+}
+
+novoUser("Galgamet", "12345")
+
